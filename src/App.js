9c417ea3de6be55e8929225d1cf4da7507d258ec
jamalabd <jamalabdul47@gmail.com>
2021-01-02 09:24:14 -0500

    added onsubmit function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, {useState} from 'react';
 import './App.css';
+
 function App() {
-  
+  const hadleSubmit = (e)=>{
+    e.preventDefault();
+  }
+
   return ( 
     <div className="App">
       <header className="App-header">
-        <form>
+        <form onSubmit={hadleSubmit}>
           <label>
             Email:
           <input type='text' name='email'/>
@@ -15,14 +19,13 @@ function App() {
           <input type='text' name='name'/>
           </label>
           <label>
-            Password: 
+            Password:
           <input type='text' name='password'/>
           </label>
           <label>
             Submit: 
           <input type='submit' name='submit'/>
           </label>
-
         </form>
               </header>
     </div>
